f14e43a95c436d4f01a8b8333195eaf8bb1694a2
Andrew 'Necromant' Andrianov <necromant@necromant.ath.cx>
2011-06-24 22:54:13 +0400

    crumbo: fix adc driver crumbo: add adc test app

--- src/contrib/crumbo/adc.c
+++ src/contrib/crumbo/adc.c
@@ -4,35 +4,56 @@
 // Will eventually go down the closet
 #include <arch/antares.h>
 #include "include/adc.h"
+
+#include "include/debug.h"
+
 #define CHAN_COUNT 16
 
 static PROGMEM char channels[CHAN_COUNT]= { 
-1,  2,  3,  4,  5,  6,  7,  8, 9, 10, 11,
+0,  1,  2,  3,  4,  5,  6,  7, 8, 9, 10, 11,
 0x00 };
 
-static int ch;
-static char channel_data[9];
+static volatile int ch;
+static volatile uint16_t channel_data[CHAN_COUNT];
+
+#define VREFS (1<<REFS0)
+
 ANTARES_INIT_LOW(adc_init)
 {
     ADCSRA|= (1<< ADPS2) | (1<<ADPS1) | (1<<ADPS0) | (1<<ADIE) | (1 << ADEN); //125KHZ
-    ADMUX |= (1<<REFS0) | (1 << ADLAR);
+    ADMUX |= VREFS;
     ADCSRA|=(1<<ADSC);
+    //ADCSRA|=(1<<ADATE);
 
 } // __inline void adc_init()
 ISR(ADC_vect) {
-    channel_data[ch]=ADCH;
-    switch(ch) {
-    }//switch(ch)
+    channel_data[ch]=ADCW;
+    //switch(ch) {
+    //}//switch(ch)
     ADMUX&=~(0x1f);
-    ADMUX|=(0xe0 & pgm_read_byte(channels+ch));
-    ADCSRB|=(MUX5 & pgm_read_byte(channels+ch));
-    if (ch!=CHAN_COUNT)
+    if (ch<CHAN_COUNT)
         ++ch;
     else
         ch=0;
+    ADMUX=(VREFS | pgm_read_byte(channels+ch));
+    ADCSRB|=(MUX5 & pgm_read_byte(channels+ch));
     ADCSRA|=(1<<ADSC);
 }
 __inline uint8_t adc_get_data(int channel)
 {
     return channel_data[channel];
 } //
+
+
+#ifdef CONFIG_CONTRIB_CRUMBO_ADC_TEST
+
+ANTARES_APP(adc_test)
+{
+ int i;
+ for (i=0; i< CHAN_COUNT; i++)
+ {
+   dump16(channel_data[i]);
+ }
+ _delay_ms(1000);
+}
+#endif
