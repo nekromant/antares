d37e3a7dd7816c0588953e9e971612f01479f221
Andrew 'Necromant' Andrianov <necromant@necromant.ath.cx>
2011-07-18 21:31:15 +0400

    deploy: added avrdude programmers lists arm: dummies for stm32 and stellaris

---
+++ deploy/avrdude/proglist.mk
@@ -0,0 +1,250 @@
+# Automatically generated. 
+# I would't edit if I were you
+ifeq ($(CONFIG_AVRDUDE_PROG_C2N232I),y)
+PROG=c2n232i
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_DASA3),y)
+PROG=dasa3
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_DASA),y)
+PROG=dasa
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_SIPROG),y)
+PROG=siprog
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_PONYSER),y)
+PROG=ponyser
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_89ISP),y)
+PROG=89isp
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_FRANK-STK200),y)
+PROG=frank-stk200
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_BLASTER),y)
+PROG=blaster
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_ERE-ISP-AVR),y)
+PROG=ere-isp-avr
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_ATISP),y)
+PROG=atisp
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_DAPA),y)
+PROG=dapa
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_XIL),y)
+PROG=xil
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_FUTURLEC),y)
+PROG=futurlec
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_ABCMINI),y)
+PROG=abcmini
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_PICOWEB),y)
+PROG=picoweb
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_SP12),y)
+PROG=sp12
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_ALF),y)
+PROG=alf
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_BASCOM),y)
+PROG=bascom
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_DT006),y)
+PROG=dt006
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_PONY-STK200),y)
+PROG=pony-stk200
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_STK200),y)
+PROG=stk200
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_BSD),y)
+PROG=bsd
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_PAVR),y)
+PROG=pavr
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_DRAGON_PDI),y)
+PROG=dragon_pdi
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_DRAGON_DW),y)
+PROG=dragon_dw
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_DRAGON_HVSP),y)
+PROG=dragon_hvsp
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_DRAGON_PP),y)
+PROG=dragon_pp
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_DRAGON_ISP),y)
+PROG=dragon_isp
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_DRAGON_JTAG),y)
+PROG=dragon_jtag
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_JTAG2PDI),y)
+PROG=jtag2pdi
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_JTAG2AVR32),y)
+PROG=jtag2avr32
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_JTAGMKII_AVR32),y)
+PROG=jtagmkII_avr32
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_JTAG2DW),y)
+PROG=jtag2dw
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_JTAG2ISP),y)
+PROG=jtag2isp
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_JTAG2),y)
+PROG=jtag2
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_JTAG2FAST),y)
+PROG=jtag2fast
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_JTAG2SLOW),y)
+PROG=jtag2slow
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_JTAGMKII),y)
+PROG=jtagmkII
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_JTAG1SLOW),y)
+PROG=jtag1slow
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_JTAG1),y)
+PROG=jtag1
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_JTAGMKI),y)
+PROG=jtagmkI
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_AVR911),y)
+PROG=avr911
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_AVR109),y)
+PROG=avr109
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_BUTTERFLY),y)
+PROG=butterfly
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_USBTINY),y)
+PROG=usbtiny
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_USBASP),y)
+PROG=usbasp
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_AVR910),y)
+PROG=avr910
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_STK600HVSP),y)
+PROG=stk600hvsp
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_STK600PP),y)
+PROG=stk600pp
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_STK600),y)
+PROG=stk600
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_STK500HVSP),y)
+PROG=stk500hvsp
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_STK500PP),y)
+PROG=stk500pp
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_STK500V2),y)
+PROG=stk500v2
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_MIB510),y)
+PROG=mib510
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_STK500V1),y)
+PROG=stk500v1
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_STK500),y)
+PROG=stk500
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_BUSPIRATE),y)
+PROG=buspirate
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_AVRISP2),y)
+PROG=avrisp2
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_AVRISPMKII),y)
+PROG=avrispmkII
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_AVRISPV2),y)
+PROG=avrispv2
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_AVRISP),y)
+PROG=avrisp
+endif
+
+ifeq ($(CONFIG_AVRDUDE_PROG_ARDUINO),y)
+PROG=arduino
+endif
+
