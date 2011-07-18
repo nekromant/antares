871cf8f37480a14010b8c578e2a4852a088f7585
Andrew 'Necromant' Andrianov <necromant@necromant.ath.cx>
2011-07-18 15:09:46 +0400

    msp430: startup.c and antares.h implementation

---
+++ src/arch/msp430/include/antares.h
@@ -0,0 +1,47 @@
+//Top level header
+//This sucks in the mcu-headers
+#ifndef _ANTARES_H
+#define _ANTARES_H
+#include <generated/autoconf.h>
+
+#ifdef CONFIG_ARCH_MSP430
+//Suck in all the default headers
+#include <string.h>
+#include <io.h>
+#include <signal.h>
+//#include <string.h>
+
+//In case of avr we use .initX sections for low and high init functions
+
+//Suck in all the default headers
+
+
+//In case of avr we use .initX sections for low and high init functions
+//APPS are called one by one in the main()
+#define ANTARES_INIT_LOW(fn) \
+ __attribute__((naked))\
+__attribute__((__section__(".init5"))) void fn(void)
+
+#define ANTARES_INIT_HIGH(fn) \
+  __attribute__((naked))\
+__attribute__((__section__(".init7"))) void fn(void)
+
+//TODO: Move this shit away once we have multitasking
+#define ANTARES_APP(fn) \
+  __attribute__((naked))\
+__attribute__((__section__(".init8"))) void fn(void)
+
+//This is a dummy define, ANTARES_LINK_RULE will be parsed 
+//via bash to determine link order
+#define ANTARES_LINK_RULE(foo)  
+
+#define ANTARES_FINISH(fn) \
+  __attribute__((naked))\
+__attribute__((__section__(".fini0"))) void fn(void)
+
+
+
+#endif
+
+
+#endif
