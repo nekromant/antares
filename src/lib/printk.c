80c8d36dc550c1ef26ac039a7b5ec7a6e71ba851
Andrew 'Necromant' Andrianov <necromant@necromant.ath.cx>
2012-04-08 12:10:41 +0400

    lib: added printk draft

---
+++ src/lib/printk.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <lib/tasks.h>
+
+void printk(const char *fmt, /*args*/ ...)
+{
+	printf("[%d	] ", tmgr_get_uptime());
+	va_list ap;
+	va_start(ap, fmt); 
+	vprintf(fmt,ap);
+	va_end(ap);
+}
