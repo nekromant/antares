6f9f8184d72479956f5370cf5224b885e090a390
DmitrySU <dsuhotsk@gmail.com>
2012-04-13 21:22:54 +0400

    (app) added Hatul-Madan robot source code

---
+++ src/app/hatul-madan.c
@@ -0,0 +1,91 @@
+#include <stm32f10x.h>
+#include <stm32f10x_gpio.h>
+#include <stm32f10x_adc.h>
+#include <stm32f10x_tim.h>
+#include <stm32f10x_rcc.h>
+
+#include "hm_chassis.h"
+
+static GPIO_InitTypeDef motors_dir = {
+  .GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15,
+  .GPIO_Speed = GPIO_Speed_50MHz,
+  .GPIO_Mode = GPIO_Mode_Out_PP
+};
+
+static GPIO_InitTypeDef motors_pwm = {
+  .GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7,
+  .GPIO_Speed = GPIO_Speed_50MHz,
+  .GPIO_Mode = GPIO_Mode_AF_PP
+};
+
+static TIM_TimeBaseInitTypeDef pwm_timer = {
+  .TIM_Prescaler = 16,
+  .TIM_CounterMode = TIM_CounterMode_Up,
+  .TIM_Period = 65535,
+  .TIM_ClockDivision = 0,
+}
+
+static TIM_OCInitTypeDef pwm_outs = {
+  .TIM_OCMode = TIM_OCMode_PWM1,
+  .TIM_OutputState = TIM_OutputState_Enable,
+  .TIM_OCPolarity = TIM_OCPolarity_High,
+  .TIM_Pulse = 0
+}  
+
+inline void init_gpio(void)
+{
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+  
+  GPIO_Init(GPIOB, &motors_dir);
+  GPIO_Init(GPIOC, &motors_pwm);
+}
+
+inline void init_timer(void)
+{
+  // Set clock to AFIO
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+  // Remap TIM3 to GPIOC
+  GPIO_PinRemapConfig(GPIO_FullRemap_TIM3, ENABLE);
+  
+  // Set clock to timer
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM3, ENABLE);
+  // Init timer
+  TIM_TimeBaseInit(TIM3, &pwm_timer);
+  // Init outputs
+  TIM_OC1Init(TIM3, &pwm_outs);
+  TIM_OC2Init(TIM3, &pwm_outs);
+  
+  TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
+  TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
+  
+  // Run timer
+  TIM_Cmd(TIM3, ENABLE);
+}
+
+inline void init_adc(void)
+{
+  
+}
+
+int main(void)
+{
+  init_gpio();
+  init_timer();
+  init_adc();
+  
+  int i, c;
+  
+  for(i=0; i<65536; i+=127)
+  {
+    chassis_write(forward, forward, i, i);
+    for(c=0; c<299999; c++)
+    {
+      asm("nop");
+    }
+  }
+  
+  while(1);;;
+  
+  return 0;
+}
---
+++ src/app/hm_chassis.c
@@ -0,0 +1,74 @@
+#include "hm_chassis.h"
+
+#include <stm32f10x.h>
+#include <stm32f10x_gpio.h>
+#include <stm32f10x_tim.h>
+
+void motor_set_dir(motor_t motor, dir_t dir)
+{
+  if(motor == left)
+  {
+    if(dir == forward)
+    {
+      GPIO_SetBits(GPIOB, GPIO_Pin_14);
+      GPIO_ResetBits(GPIOB, GPIO_Pin_15);
+    }
+    else
+    {
+      GPIO_SetBits(GPIOB, GPIO_Pin_15);
+      GPIO_ResetBits(GPIOB, GPIO_Pin_14);
+    }
+  }
+  else
+  {
+    if(dir == forward)
+    {
+      GPIO_SetBits(GPIOB, GPIO_Pin_13);
+      GPIO_ResetBits(GPIOB, GPIO_Pin_12);
+    }
+    else
+    {
+      GPIO_SetBits(GPIOB, GPIO_Pin_12);
+      GPIO_ResetBits(GPIOB, GPIO_Pin_13);
+    }
+  }
+}
+
+void motor_set_speed(motor_t motor, uint16_t speed)
+{
+  if(motor == left)
+  {
+    TIM_SetCompare1(TIM3, speed);
+  }
+  else
+  {
+    TIM_SetCompare2(TIM3, speed);
+  }
+}
+
+void motor_stop(motor_t motor)
+{
+  if(motor == left)
+  {
+    GPIO_ResetBits(GPIOB, GPIO_Pin_14 | GPIO_Pin_15);
+  }
+  else
+  {
+    GPIO_ResetBits(GPIOB, GPIO_Pin_12 | GPIO_Pin_13);
+  }
+}
+
+/////////////////////////////////////////////////////////////////
+
+void chassis_write(dir_r dir1, dir_t dir2, uint16_t speed1, uint16_t speed2)
+{
+  motor_set_dir(left, dir1);
+  motor_set_dir(right, dir2);
+  motor_set_speed(left, speed1);
+  motor_set_speed(right, speed2);
+}
+
+void chassis_stop(void)
+{
+  GPIO_ResetBits(GPIOB, GPIO_Port_12 | GPIO_Port_13 | GPIO_Port_14 | GPIO_Port_15);
+}
