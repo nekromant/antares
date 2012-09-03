5c13f205f6d3017a6307fb4a3fb7b35143d36391
Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>
2012-09-03 14:29:30 +0400

    sdcc: include properly autoconf.h sdcc: generate dependencies
    Signed-off-by: Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>

--- toolchains/sdcc.mk
+++ toolchains/sdcc.mk
@@ -10,28 +10,25 @@ O=rel
 
 COMPILER_TOOLS=CC="$(CC)" CXX="$(CXX)" LD="$(LD)" AR="$(AR)" AS="$(AS)" OBJCOPY="$(OBJCOPY)" OBJDUMP="$(OBJDUMP)" DISAS="$(DISAS)" SIZE="$(SIZE)"
 
+GENDEPFLAGS=-Wp "-MD -MP -MF $$(@).d"
 
-GENDEPFLAGS= -Wp "-MD -MP -MF $(OBJDIR)/$(@).d"
-
-#sdcc does NOT support -include directive. Bad
-
+CFLAGS+=-Wp "-include $(TOPDIR)/include/generated/autoconf.h"
 CFLAGS+=$(call unquote,$(CONFIG_SDCC_CFLAGS))
-
-
 LDFLAGS+=$(call unquote,$(CONFIG_SDCC_LDFLAGS))
 
 #TODO: Move this to subarch makefiles, and hook to kconfig
-CFLAGS+=-mmcs51
-CFLAGS+=--model-small
-CFLAGS+=--code-loc 0x2000 --data-loc 0x30 --xram-loc 0x6000
+COMMONFLAGS+=-mmcs51
+COMMONFLAGS+=--model-small
+COMMONFLAGS+=--code-loc 0x2000 --data-loc 0x30 --xram-loc 0x6000
 
 
 ASFLAGS+=$(call unquote,$(CONFIG_SDCC_ASFLAGS))
 
 
 CFLAGS+=-I$(TOPDIR)/include -I$(ANTARES_DIR)/include
+CFLAGS+=$(COMMONFLAGS) $(GENDEPFLAGS)
 ASFLAGS+=$(COMMONFLAGS) $(GENDEPFLAGS)
-CFLAGS+=$(GENDEPFLAGS)
+LDFLAGS+=$(COMMONFLAGS)
 
 export CC CXX LD AR AS OBJCOPY DISAS OBJDUMP SIZE COMPILER_TOOLS LD_NO_COMBINE
 export ASFLAGS CFLAGS LDFLAGS ELFFLAGS 
@@ -47,20 +44,11 @@ builtin:
 	-C $(TOPDIR)/$(project_sources) \
 	TMPDIR=$(TMPDIR) -f $(ANTARES_DIR)/make/Makefile.build -r build
 
-ifneq ($(LD_NO_COMBINE),y)
-$(IMAGENAME).ihx:  builtin
-	$(SILENT_LD) $(CC) $(CFLAGS) $(OBJDIR)/build/built-in.o  
-else
+
 $(IMAGENAME).ihx: builtin
-	$(SILENT_LD) $(CC) $(CFLAGS) $(LDFLAGS) -o $(@) \
+	$(SILENT_LD) $(CC) $(LDFLAGS) -o $(@) \
 	`$(ANTARES_DIR)/scripts/parseobjs $(TOPDIR)/build/built-in.rel` \
 	`$(ANTARES_DIR)/scripts/parseobjs $(TOPDIR)/build/app/built-in.rel`
-endif
 
-$(IMAGENAME).bin: $(IMAGENAME).elf
-	$(SILENT_OBJCOPY) $(OBJCOPY) $(OBJCOPYFLAGS) $< $(@) 
-
-$(IMAGENAME).lss: $(IMAGENAME).elf
-	$(SILENT_DISAS) $(OBJDUMP) -h -S $< > $@
 
 PHONY+=builtin
