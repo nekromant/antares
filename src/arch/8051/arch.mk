dee738cf93e21b539cdb30abfb824e9555821c7f
Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>
2012-09-04 20:57:46 +0400

    8051: Add STC stubs, add antares.h stub, cleanup
    Signed-off-by: Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>

--- src/arch/8051/arch.mk
+++ src/arch/8051/arch.mk
@@ -1,7 +1,16 @@
 BUILDGOALS=$(IMAGENAME).ihx
 
-# Do not combine objects into built-in.o
-# This screws up things on avr and breaks ANTARES_* macros
+# Do not combine objects into one built-in.o
+# This screws up things on avr and breaks ANTARES_* macros. Not supported by some compilers (sdcc)
 # So it's 100% safe to set this to y
+
 LD_NO_COMBINE=y
+
+COMMONFLAGS+=-mmcs51
+#TODO: menuconfigg'ed stack model and other shit
+COMMONFLAGS+=--model-small
+
+ifeq ($(CONFIG_ARCH_8051_STC),y)
+include $(ANTARES_DIR)/src/arch/8051/stc/arch.mk
+endif
 
---
+++ src/arch/8051/stc/arch.mk
@@ -0,0 +1,1 @@
+#TODO: Any extra opts for STC here
