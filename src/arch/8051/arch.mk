8aa51759b5046da9b191ddce6935fc84cd4b1fa8
Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>
2012-09-09 18:45:10 +0400

    8051: Add .bin file generation
    Since some ISP tools (like STC ISP) fail to read both .ihx and .hex properly, we'll make a bin file
    
    Signed-off-by: Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>

--- src/arch/8051/arch.mk
+++ src/arch/8051/arch.mk
@@ -1,4 +1,4 @@
-BUILDGOALS=$(IMAGENAME).ihx
+BUILDGOALS=$(IMAGENAME).ihx $(IMAGENAME).bin
 
 # Do not combine objects into one built-in.o
 # This screws up things on avr and breaks ANTARES_* macros. Not supported by some compilers (sdcc)
@@ -10,7 +10,11 @@ COMMONFLAGS+=-mmcs51
 #TODO: menuconfigg'ed stack model and other shit
 COMMONFLAGS+=--model-small
 
+
+
 ifeq ($(CONFIG_ARCH_8051_STC),y)
 include $(ANTARES_DIR)/src/arch/8051/stc/arch.mk
 endif
 
+$(IMAGENAME).bin: $(IMAGENAME).ihx
+	$(SILENT_HEX2BIN)hex2bin $(<)
