f8c9f483db4792ac80efd70afb31455d5ea0704f
Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>
2013-04-30 17:59:27 +0400

    msp430: completely redesign delaylib
    Signed-off-by: Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>

--- src/arch/msp430/include/lib/delaylib.h
+++ src/arch/msp430/include/lib/delaylib.h
@@ -1,20 +1,21 @@
 #ifndef __DELAYLIB_H
 #define __DELAYLIB_H
 
-#define CYCLES_PER_S  (CONFIG_MSP430_DELAYLIB_FRQ)
-#define CYCLES_PER_MS (CONFIG_MSP430_DELAYLIB_FRQ / 1000)
-#define CYCLES_PER_US (CONFIG_MSP430_DELAYLIB_FRQ / 1000000)
-#define CYCLES_PER_NS (CONFIG_MSP430_DELAYLIB_FRQ / 1000000000)
+void delay_s(unsigned long int s);
+void delay_ms(unsigned long int s);
 
-#ifdef CONFIG_MSP430_DELAYLIB_SCALING
-void delay_scale(int mmul, int mdiv);
+#ifndef CONFIG_MSP430_DELAYLIB_DYNAMIC
+ #define delay_us(m) __delay_cycles(m * (CONFIG_MSP430_DELAYLIB_FRQ / 1000000UL) * CYCLES_PER_US);
+ #define delaylib_set_clock(f) { }
 #else
-#define delay_scale(mmul, mdiv);
+ void delaylib_set_clock(unsigned long int f);
+ void delay_us(unsigned long int s);
 #endif
 
-void delay_s(int s);
-void delay_ms(int ms);
-void delay_us(int us);
-void delay_ns(int ns);
+#ifdef CONFIG_MSP430_DELAYLIB_SUMMARY
+void delaylib_print_summary();
+#else
+#define delaylib_print_summary() { }
+#endif
 
 #endif
--- src/lib/msp430/delaylib.c
+++ src/lib/msp430/delaylib.c
@@ -1,39 +1,64 @@
 #include <msp430.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <arch/lib/delaylib.h>
 
+#define DELAY_FUNC(macro,measure,mul)			\
+	void delay_ ## measure (unsigned long int m) {	\
+		m = m * mul;				\
+		while(m--)				\
+			__delay_cycles(macro);		\
+	}
 
-#ifdef CONFIG_MSP430_DELAYLIB_SCALING
-static int mul=1;
-static int div=1;
-void delay_scale(int mmul, int mdiv)
+
+#ifdef CONFIG_MSP430_DELAYLIB_DYNAMIC
+
+#define CYCLES_PER_UNIT (unsigned long int) CONFIG_MSP430_DELAYLIB_UNITSIZE
+static unsigned long int UNITS_PER_S;
+static unsigned long int UNITS_PER_MS;
+static unsigned long int UNITS_PER_US;
+
+void delaylib_set_clock(unsigned long int f) 
 {
-	mul=mmul;
-	div=mdiv;
+	UNITS_PER_S  = f / CYCLES_PER_UNIT;
+	UNITS_PER_MS = f / CYCLES_PER_UNIT / 1000UL;
+	UNITS_PER_US = f / CYCLES_PER_UNIT / 1000000UL; 
 }
 
-#define __SCALEM(m) m*=mul; m/=div;
+
+
+DELAY_FUNC(CYCLES_PER_UNIT,ms, UNITS_PER_MS);
+DELAY_FUNC(CYCLES_PER_UNIT,us, UNITS_PER_US);
+DELAY_FUNC(CYCLES_PER_UNIT,s, UNITS_PER_S);
+
 #else
-#define __SCALEM(m) ;
+
+#define CYCLES_PER_S  (CONFIG_MSP430_DELAYLIB_FRQ)
+#define CYCLES_PER_MS (CONFIG_MSP430_DELAYLIB_FRQ / 1000UL)
+#define CYCLES_PER_US (CONFIG_MSP430_DELAYLIB_FRQ / 1000000UL)
+
+DELAY_FUNC(CYCLES_PER_MS, ms,1);
+DELAY_FUNC(CYCLES_PER_S, s, 1);
+
+
 #endif
 
+#ifdef CONFIG_MSP430_DELAYLIB_SUMMARY
+void delaylib_print_summary()
+{
+#ifdef CONFIG_MSP430_DELAYLIB_DYNAMIC
+	printf("delaylib: running in dynamic mode\n");
+	printf("delaylib: unit size %lu cycles\n", CYCLES_PER_UNIT);
+	printf("delaylib: units_per_s %lu units_per_ms %lu units_per_us %lu\n",
+	       UNITS_PER_S, UNITS_PER_MS, UNITS_PER_US);
+#else
+	printf("delaylib: running in static mode\n");
+	printf("delaylib: cycles_per_s %lu cycles_per_ms %lu cycles_per_us %lu\n",
+	       CYCLES_PER_S, CYCLES_PER_MS, CYCLES_PER_US);
+#endif
+#endif
 
-#define DELAY_FUNC(macro,measure) \
-void delay_ ## measure (int m) {\
-	__SCALEM(m);			\
-	while(m--)			\
-		__delay_cycles(macro);	\
 }
-
-DELAY_FUNC(CYCLES_PER_MS,ms);
-DELAY_FUNC(CYCLES_PER_MS,us);
-DELAY_FUNC(CYCLES_PER_MS,ns);
-DELAY_FUNC(CYCLES_PER_MS,s);
-
-
-
-
-
-
 
 
 
