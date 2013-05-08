50e443634f50ac1163de53291f29cf2a2e8f38e7
Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>
2013-05-08 21:43:09 +0400

    printk: rework code
    Signed-off-by: Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>

--- include/lib/printk.h
+++ include/lib/printk.h
@@ -1,12 +1,51 @@
 #ifndef PRINTK_H
 #define PRINTK_H
 
-#ifdef DEBUG
-#define dbg(...) printk(__VA_ARGS__)
+#ifndef DEBUG_LEVEL
+#define DEBUG_LEVEL 4
+#endif
+ 
+#ifndef COMPONENT
+#define COMPONENT "???"
 #endif
 
-#ifdef CONFIG_LIB_PRINTK_TIMESTAMP
-void printk(const char *fmt, /*args*/ ...);
+#define log(level, ...) __dbg_ # level (__VA_ARGS__)
+
+#define err(...) printk(COMPONENT ": " __VA_ARGS__)
+#define warn(...) __dbg_0(__VA_ARGS__)
+#define info(...) __dbg_1(__VA_ARGS__)
+#define dbg(...) __dbg_2(__VA_ARGS__)
+
+
+
+#if DEBUG_LEVEL > 0 
+#define __dbg_0(...) printk(COMPONENT ": " __VA_ARGS__)
+#define D_WARN_ENABLED
+#else
+#define __dbg_0(...) { }
 #endif
+
+#if DEBUG_LEVEL > 1 
+#define __dbg_1(...) printk(COMPONENT ": " __VA_ARGS__)
+#define D_INFO_ENABLED
+#else
+#define __dbg_1(...) { }
+#endif
+
+#if DEBUG_LEVEL > 2 
+#define __dbg_2(...) printk(COMPONENT ": " __VA_ARGS__)
+#define D_DBG_ENABLED
+#else
+#define __dbg_2(...) { }
+#endif
+
+#if DEBUG_LEVEL > 3 
+#define __dbg_3(...) printk(COMPONENT ": " __VA_ARGS__)
+#else
+#define __dbg_3(...) { }
+#endif
+
+
+void printk(const char *fmt, /*args*/ ...);
 
 #endif
--- src/lib/printk.c
+++ src/lib/printk.c
@@ -5,16 +5,20 @@
 #include <lib/tasks.h>
 
 
-#ifdef CONFIG_LIB_PRINTK_TIMESTAMP
 void printk(const char *fmt, /*args*/ ...)
 {
-
+#ifdef CONFIG_LIB_PRINTK_PREFIX
+	printf(CONFIG_LIB_PRINTK_PREFIX_V);
+#endif
+	
+#ifdef CONFIG_LIB_PRINTK_TIMESTAMP
 	printf("[%d	] ", tmgr_get_uptime());
+#endif
 	va_list ap;
 	va_start(ap, fmt); 
 	vprintf(fmt,ap);
 	va_end(ap);
 }
 
-#endif
+
 
