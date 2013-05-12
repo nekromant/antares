0211709f99c44b31bdd06f2e58c14eae34527f94
Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>
2013-05-13 03:58:27 +0400

    stm32: properly enable/disable IRQs in init code
    Signed-off-by: Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>

--- src/arch/arm/stm32/dummy.c
+++ src/arch/arm/stm32/dummy.c
@@ -1,5 +1,15 @@
 #include <arch/antares.h>
+#include "misc.h"
 
+ANTARES_INIT_LOW(__disable_irqs__) 
+{
+	__disable_irq();
+}
+
+ANTARES_INIT_HIGH(__enable_irqs) 
+{
+	__enable_irq();
+}
 
 /* Dummy. 
  * Required to cause link errors if antares startup code 
