174d15e83be4df8871baa577add68f922385d027
Nikita 'webconn' Maslov <wkernelteam@gmail.com>
2013-10-19 07:19:18 +0400

    Rework tmgr library (issue #16) NEED TESTING
    Features of reworked tmgr library:
    
    * Cross-platform, tiny task manager/scheduler
    
    * Two modes of operation: uptime-based (faster, but limited in device
    uptime) and countdown-based (a bit slower, but uptime-independent)
    
    * In uptime-based mode, the information about system uptime is available
    and could be used in projects (for debugging, logging etc.)
    
    * Time conversion functions (s/ms/us to timer ticks and vice versa)
    
    * Basic delay implementation
    
    Signed-off-by: Nikita 'webconn' Maslov <wkernelteam@gmail.com>

--- include/lib/tmgr.h
+++ include/lib/tmgr.h
@@ -1,38 +1,48 @@
 #ifndef INCLUDE_LIB_TMGR_H
 #define INCLUDE_LIB_TMGR_H
 
-#ifdef CONFIG_ARCH_NATIVE
-    #include <stdlib.h>
-    #include <inttypes.h>
-#endif
+#include "inttypes.h"
 
 #ifndef NULL
     #define NULL 0
 #endif
 
-/**
- * TODO: insert platform-dependent includes
- */
+#if defined(CONFIG_LIB_TMGR_TIME_16)
+typedef int16_t tmgr_time_t;
+#elif defined(CONFIG_LIB_TMGR_TIME_32)
+typedef int32_t tmgr_time_t;
+#elif defined(CONFIG_LIB_TMGR_TIME_64)
+typedef int64_t tmgr_time_t;
+#else
+    // TODO: format errror message
+    #error "Wrong tmgr_time_t size (must be 16, 32 or 64 bits)"
+#endif
 
-/**
- * TODO: uptime_t should be platform-dependent (or application-dependent)
- */
-typedef int32_t uptime_t;
-
-typedef struct struct_task_t{
-    uptime_t time;
+typedef struct struct_tmgr_task_t{
+    tmgr_time_t time;
     void (*func)(void);
-    struct struct_task_t * next;
-} task_t;
+    struct struct_tmgr_task_t * next;
+} tmgr_task_t;
 
 void tmgr_tick(void);
 void tmgr_timer(void);
-void tmgr_register(task_t * task);
+void tmgr_register(tmgr_task_t * task, void (*function)(void), tmgr_time_t delay);
 
-uptime_t tmgr_get_uptime(void);
-void tmgr_delay(uptime_t time);
+#ifdef CONFIG_LIB_TMGR_UPTIME
+tmgr_time_t tmgr_get_uptime(void);
+#endif
+void tmgr_delay(tmgr_time_t time);
 
-void tmgr_set_fq(int32_t fq);
-int32_t tmgr_get_fq(void);
+void tmgr_set_fq(tmgr_time_t fq);
+tmgr_time_t tmgr_get_fq(void);
+
+#ifdef CONFIG_LIB_TMGR_EXTRA
+tmgr_time_t tmgr_ticks_to_us(tmgr_time_t ticks);
+tmgr_time_t tmgr_ticks_to_ms(tmgr_time_t ticks);
+tmgr_time_t tmgr_ticks_to_s(tmgr_time_t ticks);
+tmgr_time_t tmgr_us_to_ticks(tmgr_time_t us);
+tmgr_time_t tmgr_ms_to_ticks(tmgr_time_t ms);
+tmgr_time_t tmgr_s_to_ticks(tmgr_time_t s);
+#endif
 
 #endif
--- src/lib/ulc/tmgr.c
+++ src/lib/ulc/tmgr.c
@@ -1,10 +1,14 @@
 #include <lib/tmgr.h>
 
-static volatile uptime_t sys_uptime = 0;
-static volatile int32_t sys_fq = 0;
+static volatile tmgr_time_t sys_uptime = 0;
+static volatile int32_t sys_fq = 1;
 
-static task_t *fast_queue = NULL;
-static task_t *main_queue = NULL;
+static tmgr_task_t *fast_queue = NULL;
+static tmgr_task_t *main_queue = NULL;
+
+#ifdef CONFIG_LIB_TMGR_COUNTDOWN
+static tmgr_time_t delay;
+#endif
 
 /**
  * @section Task Manager core
@@ -13,14 +17,35 @@ static task_t *main_queue = NULL;
 
 void tmgr_timer(void)
 {
+#if defined(CONFIG_LIB_TMGR_UPTIME)
     sys_uptime++;
+#elif defined(CONFIG_LIB_TMGR_COUNTDOWN)
+    // Implement delay feature
+    if(delay > 0)
+        delay--;
+
+    tmgr_task_t * queue = main_queue;
+    while(queue != NULL)
+    {
+        queue->time --;
+        queue = queue->next;
+    }
+#endif
 }
 
 // TODO: make it atomic
-void tmgr_register(task_t * task)
+void tmgr_register(tmgr_task_t * task, void (*function)(void), tmgr_time_t delay)
 {
+#if defined(CONFIG_LIB_TMGR_UPTIME)
+    task->time = sys_uptime + delay;
+#elif defined(CONFIG_LIB_TMGR_COUNTDOWN)
+    task->time = delay;
+#endif
+    task->func = function;
+    // these lines MUST be atomic {
     task->next = fast_queue;
     fast_queue = task;
+    // }
 }
 
 void tmgr_tick(void)
@@ -29,7 +54,8 @@ void tmgr_tick(void)
     if(fast_queue != NULL)
     {
         // Attach fast queue to main queue
-        task_t *fast = fast_queue,
+        // TODO: from this moment to `fast_queue = NULL` lines must be atomic!
+        tmgr_task_t *fast = fast_queue,
                *current = main_queue,
                **last = &main_queue;
 
@@ -37,7 +63,11 @@ void tmgr_tick(void)
 
         while(fast != NULL)
         {
+            #if defined(CONFIG_LIB_TMGR_UPTIME)
             while(current != NULL && current->time < fast->time)
+            #elif defined(CONFIG_LIB_TMGR_COUNTDOWN)
+            while(current != NULL && current->time > fast->time)
+            #endif
             {
                 current = current->next;
                 last = &((*last)->next);
@@ -53,7 +83,11 @@ void tmgr_tick(void)
     // 2. Run main queue
     if(main_queue != NULL)
     {
-        while(main_queue != NULL && main_queue->time <= sys_uptime)
+        #if defined(CONFIG_LIB_TMGR_UPTIME)
+        while(main_queue != NULL && main_queue->time <= sys_uptime) 
+        #elif defined(CONFIG_LIB_TMGR_COUNTDOWN)
+        while(main_queue != NULL && main_queue->time <= 0)
+        #endif
         {
             main_queue->func();
             main_queue = main_queue->next;
@@ -68,65 +102,72 @@ void tmgr_tick(void)
  * @{
  */
 
-uptime_t tmgr_get_uptime(void)
+tmgr_time_t tmgr_get_uptime(void)
 {
     return sys_uptime;
 }
 
-void tmgr_delay_ticks(uptime_t time)
+void tmgr_delay(tmgr_time_t time)
 {
+#if defined(CONFIG_LIB_TMGR_UPTIME)
     time += sys_uptime;
-
     while(time > sys_uptime);
+#elif defined(CONFIG_LIB_TMGR_COUNTDOWN)
+    delay = time;
+    while(delay > 0);
+#endif
 }
 
 /**
  * @}
  */
 
+#ifdef CONFIG_LIB_TMGR_EXTRA
 /**
  * @section Time conversion functions
  * @{
  */
-void tmgr_set_fq(int32_t fq)
+void tmgr_set_fq(tmgr_time_t fq)
 {
     sys_fq = fq;
 }
 
-int32_t tmgr_get_fq(void)
+tmgr_time_t tmgr_get_fq(void)
 {
     return sys_fq;
 }
 
-int32_t tmgr_ticks_to_us(int32_t ticks)
+tmgr_time_t tmgr_ticks_to_us(tmgr_time_t ticks)
 {
     return 1000000U * ticks / sys_fq;
 }
 
-int32_t tmgr_ticks_to_ms(int32_t ticks)
+tmgr_time_t tmgr_ticks_to_ms(tmgr_time_t ticks)
 {
     return 1000 * ticks / sys_fq;
 }
 
-int32_t tmgr_ticks_to_s(int32_t ticks)
+tmgr_time_t tmgr_ticks_to_s(tmgr_time_t ticks)
 {
     return ticks / sys_fq;
 }
 
-int32_t tmgr_us_to_ticks(int32_t us)
+tmgr_time_t tmgr_us_to_ticks(tmgr_time_t us)
 {
     return us * sys_fq / 1000000U;
 }
 
-int32_t tmgr_ms_to_ticks(int32_t ms)
+tmgr_time_t tmgr_ms_to_ticks(tmgr_time_t ms)
 {
     return ms * sys_fq / 1000;
 }
 
-int32_t tmgr_s_to_ticks(int32_t s)
+tmgr_time_t tmgr_s_to_ticks(tmgr_time_t s)
 {
     return s * sys_fq;
 }
+
+#endif
 /**
  * @}
  */
