1893df5fa00d6afeba86ff678f2547f1aa232d2c
Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>
2013-10-27 16:00:59 +0400

    xmodem: update spec draft
    Signed-off-by: Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>

--- include/lib/xmodem.h
+++ include/lib/xmodem.h
@@ -6,6 +6,7 @@ struct xmodem_receiver {
 	int (*getchar)(void); 
 	void (*putchar)(char c);
 	int (*char_avail)();
+	void delay_1s(); 
 	int (*writer)(char* buffer, int size);
 };
 
@@ -13,6 +14,7 @@ struct xmodem_transmitter {
 	int (*getchar)(void); 
 	void (*putchar)(char c);
 	int (*char_avail)();
+	void delay_1s(); 
 	int (*reader)(char* buffer, int size);
 };
 
