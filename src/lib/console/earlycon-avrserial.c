381643b2d4e2a808f2b5a965179bdad45282e0f2
Andrew Andrianov <andrew@ncrmnt.org>
2013-10-29 21:12:28 +0400

    earlycon: Really enable RX/TX in avrserial earlycon
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/lib/console/earlycon-avrserial.c
+++ src/lib/console/earlycon-avrserial.c
@@ -9,24 +9,30 @@
 
 #if defined(UDR)
 /* We have only one serial port */
-#define REG_UDR UDR
+#define REG_UDR   UDR
 #define REG_UCSRA UCSRA
+#define REG_UCSRB UCSRB
 #define REG_UBRRH UBRRH
 #define REG_UBRRL UBRRL
-#define BIT_TXC TXC
-#define BIT_U2X U2X
-#define BIT_RXC RXC
-#define BIT_UDRE UDRE
+#define BIT_TXC   TXC
+#define BIT_U2X   U2X
+#define BIT_RXC   RXC
+#define BIT_UDRE  UDRE
+#define BIT_RXEN  RXEN
+#define BIT_TXEN  TXEN
 #else
 /* We use Nth port */
 #define REG_UDR PPCAT(UDR, SERIAL_NUM)
 #define REG_UCSRA PPCAT3(UCSR, SERIAL_NUM, A)
+#define REG_UCSRB PPCAT3(UCSR, SERIAL_NUM, B)
 #define REG_UBRRH PPCAT3(UBRR, SERIAL_NUM, H)
 #define REG_UBRRL PPCAT3(UBRR, SERIAL_NUM, L)
 #define BIT_TXC PPCAT(TXC, SERIAL_NUM)
 #define BIT_U2X PPCAT(U2X, SERIAL_NUM)
 #define BIT_RXC PPCAT(RXC, SERIAL_NUM)
 #define BIT_UDRE PPCAT(UDRE, SERIAL_NUM)
+#define BIT_RXEN  PPCAT(RXEN, SERIAL_NUM)
+#define BIT_TXEN  PPCAT(TXEN, SERIAL_NUM)
 #endif
 
 
@@ -57,7 +63,7 @@ static void serial_init(void)
 #else
 	REG_UCSRA &= ~(1 << BIT_U2X);
 #endif
-
+	REG_UCSRB = (1<<BIT_RXEN)|(1<<BIT_TXEN);
 #ifdef CONFIG_LIB_EARLYCON_ANNOUNCE
 	printk("earlycon: using avr serial port %u at %u bps\n", 
 	       (unsigned int) SERIAL_NUM, 
