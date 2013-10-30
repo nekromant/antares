2747107cc41a33836fba6f633611f65218d86a19
Andrew Andrianov <andrew@ncrmnt.org>
2013-10-31 00:29:02 +0400

    msp430: Use F_CPU for initial frequency
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/lib/msp430/delaylib.c
+++ src/lib/msp430/delaylib.c
@@ -26,18 +26,17 @@ void delaylib_set_clock(unsigned long int f)
 }
 
 
-
 DELAY_FUNC(CYCLES_PER_UNIT,ms, UNITS_PER_MS);
 DELAY_FUNC(CYCLES_PER_UNIT,us, UNITS_PER_US);
 DELAY_FUNC(CYCLES_PER_UNIT,s, UNITS_PER_S);
 
 #else
 
-#define CYCLES_PER_S  (CONFIG_MSP430_DELAYLIB_FRQ)
-#define CYCLES_PER_MS (CONFIG_MSP430_DELAYLIB_FRQ / 1000UL)
-#define CYCLES_PER_US (CONFIG_MSP430_DELAYLIB_FRQ / 1000000UL)
+#define CYCLES_PER_S  (F_CPU)
+#define CYCLES_PER_MS (F_CPU / 1000UL)
+#define CYCLES_PER_US (F_CPU / 1000000UL)
 
-DELAY_FUNC(CYCLES_PER_MS, ms,1);
+DELAY_FUNC(CYCLES_PER_MS, ms, 1);
 DELAY_FUNC(CYCLES_PER_S, s, 1);
 
 
@@ -47,13 +46,13 @@ DELAY_FUNC(CYCLES_PER_S, s, 1);
 void delaylib_print_summary()
 {
 #ifdef CONFIG_MSP430_DELAYLIB_DYNAMIC
-	printf("delaylib: running in dynamic mode\n");
-	printf("delaylib: unit size %lu cycles\n", CYCLES_PER_UNIT);
-	printf("delaylib: units_per_s %lu units_per_ms %lu units_per_us %lu\n",
+	printk("delaylib: running in dynamic mode\n");
+	printk("delaylib: unit size %lu cycles\n", CYCLES_PER_UNIT);
+	printk("delaylib: units_per_s %lu units_per_ms %lu units_per_us %lu\n",
 	       UNITS_PER_S, UNITS_PER_MS, UNITS_PER_US);
 #else
-	printf("delaylib: running in static mode\n");
-	printf("delaylib: cycles_per_s %lu cycles_per_ms %lu cycles_per_us %lu\n",
+	printk("delaylib: running in static mode\n");
+	printk("delaylib: cycles_per_s %lu cycles_per_ms %lu cycles_per_us %lu\n",
 	       CYCLES_PER_S, CYCLES_PER_MS, CYCLES_PER_US);
 #endif
 }
