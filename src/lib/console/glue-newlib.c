db2ee53f9e2d16e2e1c44418f062a8c253371b92
Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>
2013-11-04 20:36:15 +0400

    earlycon: _IONBF in newlib glue
    Signed-off-by: Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>

--- src/lib/console/glue-newlib.c
+++ src/lib/console/glue-newlib.c
@@ -32,36 +32,26 @@ static ssize_t cookie_write_function(void *_cookie, const char *_buf, size_t n)
 }
 
 
-static int cookie_seek_function(void *cookie, off_t *off, int whence)
-{
-    errno = -1;
-    return -1;
-}
-
 static int cookie_close_function(void *cookie)
 {
-    return 0;
+	return 0;
 }
 
- 
-#define send(text) \
-	stlinky_tx(&g_stlinky_term, text, strlen(text)) 
 void early_console_glue_init() {
 	cookie_io_functions_t ops_read = {
 		.read = cookie_read_function,
 		.write = NULL, 
-		.seek = cookie_seek_function,
+		.seek = NULL,
 		.close = cookie_close_function
 	};
 	cookie_io_functions_t ops_write = {
 		.read = NULL,
 		.write = cookie_write_function,
-		.seek = cookie_seek_function,
-		.close = cookie_close_function
-
-	};
+		.seek = NULL,
+		.close = cookie_close_function	
+	};	
 	g_early_stdin  = fopencookie(NULL, "r", ops_read);
 	g_early_stdout = fopencookie(NULL, "w", ops_write);
-	setvbuf(g_early_stdout, NULL, _IOLBF, 1);
-        setvbuf(g_early_stdin,  NULL, _IOLBF, 1);
+	setvbuf(g_early_stdout, NULL, _IONBF, 0);
+        setvbuf(g_early_stdin,  NULL, _IONBF, 0);
 }
