fe2b8168866806a3cfbee2d69e66ddeaf791f048
Andrew Andrianov <andrew@ncrmnt.org>
2013-12-12 00:03:28 +0400

    printk: minor adjustments
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- include/lib/printk.h
+++ include/lib/printk.h
@@ -11,10 +11,11 @@
 
 #define do_log(level, ...) __dbg_ # level (__VA_ARGS__)
 
-#define err(...) printk(COMPONENT ": " __VA_ARGS__)
-#define warn(...) __dbg_0(__VA_ARGS__)
-#define info(...) __dbg_1(__VA_ARGS__)
-#define dbg(...) __dbg_2(__VA_ARGS__)
+#define err(...)       printk(COMPONENT ": " __VA_ARGS__)
+#define warn(...)      __dbg_0(__VA_ARGS__)
+#define info(...)      __dbg_1(__VA_ARGS__)
+#define dbg(...)       __dbg_2(__VA_ARGS__)
+#define trace(...)     __dbg_3(__VA_ARGS__)
 
 
 
