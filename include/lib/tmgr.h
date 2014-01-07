c925746c6fcd2210b23b2427cbead352142ad2b6
Nikita 'webconn' Maslov <wkernelteam@gmail.com>
2014-01-07 23:09:46 +0400

    tmgr: Full rework of timers library
    Now structure is a bit closer to Linux kernel timers.
    
    See docs for further information.
    
    Signed-off-by: Nikita 'webconn' Maslov <wkernelteam@gmail.com>

--- include/lib/tmgr.h
+++ include/lib/tmgr.h
@@ -5,14 +5,18 @@
 #include <arch/antares.h>
 #include <generic/macros.h>
 
-#if defined(CONFIG_LIB_TMGR_TIME_16)
-typedef int16_t tmgr_time_t;
-#elif defined(CONFIG_LIB_TMGR_TIME_32)
-typedef int32_t tmgr_time_t;
+/* TODO: remove it ASAP */
+#ifndef ANTARES_ENABLE_IRQS
+#define ANTARES_ENABLE_IRQS()
+#define ANTARES_DISABLE_IRQS()
+#endif
+
+#if defined(CONFIG_LIB_TMGR_TIME_32)
+typedef int32_t tmgr_uptime_t;
 #elif defined(CONFIG_LIB_TMGR_TIME_64)
-typedef int64_t tmgr_time_t;
+typedef int64_t tmgr_uptime_t;
 #else
-#error "tmgr: Wrong tmgr_time_t size (must be 16, 32 or 64 bits)"
+#error "tmgr: Wrong tmgr_uptime_t size (must be 32 or 64 bits)"
 #endif
 
 #define tmgr_ticks_to_us(ticks) (1000000U * (ticks) / tmgr_get_fq())
@@ -22,22 +26,38 @@ typedef int64_t tmgr_time_t;
 #define tmgr_ms_to_ticks(ms) ((ms) * tmgr_get_fq() / 1000)
 #define tmgr_s_to_ticks(s) ((s) * tmgr_get_fq())
 
-typedef struct struct_tmgr_task_t {
-        tmgr_time_t time;
-        void (*func)(void);
-        struct struct_tmgr_task_t * next;
-} tmgr_task_t;
+/* Timer initializer macros.
+ * Creates right structure for timer and fill it
+ */
+#define TMGR_TIMER_INIT(_function, _data, _expires) { .expires = (_expires), .func = (_function), .data = (_data) }
+#define TMGR_DEFINE_TIMER(_name, _function, _data, _expires) tmgr_timer_t _name = TMGR_TIMER_INIT(_function, _data, _expires)
+#define TMGR_SETUP_TIMER(_timer, _function, _data, _expires) _timer.func = (_function); _timer.data = (_data); _timer.expires = (_expires)
 
-void tmgr_tick(void);
-void tmgr_timer(void);
-void tmgr_register(tmgr_task_t * task, void (*function)(void), tmgr_time_t delay);
+#define TMGR_DELAYED(delay) (tmgr_get_uptime() + delay)
 
-#ifdef CONFIG_LIB_TMGR_UPTIME
-tmgr_time_t tmgr_get_uptime(void);
-#endif
-void tmgr_delay(tmgr_time_t time);
+/* Timers are making doubly-linked list, it's good for
+ * fast search and removal of timer object.
+ */
+typedef struct struct_tmgr_timer_t {
+        tmgr_uptime_t expires;
+        void (*func)(uint8_t *);
+        uint8_t *data;
 
-void tmgr_set_fq(tmgr_time_t fq);
-tmgr_time_t tmgr_get_fq(void);
+        struct struct_tmgr_timer_t *next;
+        struct struct_tmgr_timer_t *prev;
+} tmgr_timer_t;
+
+void tmgr_process(void);
+void tmgr_interrupt(void);
+
+void tmgr_add_timer(tmgr_timer_t *timer);
+void tmgr_mod_timer(tmgr_timer_t *timer, tmgr_uptime_t expires);
+void tmgr_del_timer(tmgr_timer_t *timer);
+
+tmgr_uptime_t tmgr_get_uptime(void);
+void tmgr_delay(tmgr_uptime_t time);
+
+void tmgr_set_fq(tmgr_uptime_t fq);
+tmgr_uptime_t tmgr_get_fq(void);
 
 #endif
--- src/lib/ulc/tmgr.c
+++ src/lib/ulc/tmgr.c
@@ -1,96 +1,144 @@
 #include <lib/tmgr.h>
 
-static volatile tmgr_time_t sys_uptime = 0;
+static volatile tmgr_uptime_t sys_uptime = 0;
 static volatile int32_t sys_fq = 1;
+static volatile uint8_t tick_parsed = 0;
 
-static tmgr_task_t *fast_queue = NULL;
-static tmgr_task_t *main_queue = NULL;
+/* Dirty-hack, but saves memory and runs on each platworm.
+ * We will use pointers to the two previous vars for
+ * "flag" pointers to check if timer list element is in
+ * main queue or in fast queue (or NULL if is not in queue)
+ */
+#define IN_MAIN (tmgr_timer_t *) (&sys_uptime)
+#define IN_FAST (tmgr_timer_t *) (&sys_fq)
 
-#ifdef CONFIG_LIB_TMGR_COUNTDOWN
-static tmgr_time_t delay;
-#endif
+static volatile tmgr_timer_t *fast_queue = NULL;
+static volatile tmgr_timer_t *main_queue = NULL;
 
 /**
  * @section Task Manager core
  * @{
  */
 
-void tmgr_timer(void)
+void tmgr_interrupt(void)
 {
-#if defined(CONFIG_LIB_TMGR_UPTIME)
         sys_uptime++;
-#elif defined(CONFIG_LIB_TMGR_COUNTDOWN)
-        /* Implement delay feature */
-        if (delay > 0)
-                delay--;
-
-        tmgr_task_t * queue = main_queue;
-        while (queue != NULL) {
-                queue->time --;
-                queue = queue->next;
-        }
-#endif
+        tick_parsed = 0;
 }
 
-void tmgr_register(tmgr_task_t * task, void (*function)(void), tmgr_time_t delay)
+void tmgr_process(void)
 {
-#if defined(CONFIG_LIB_TMGR_UPTIME)
-        task->time = sys_uptime + delay;
-#elif defined(CONFIG_LIB_TMGR_COUNTDOWN)
-        task->time = delay;
-#endif
-        task->func = function;
-        
-        /* these lines are atomic */
+        if (tick_parsed) /* this is to save time for interrupts if we had just parsed queues */
+                return;
+
+        /* Full function is atomic; accept it as it is */
         ANTARES_DISABLE_IRQS();
-        task->next = fast_queue;
-        fast_queue = task;
-        ANTARES_ENABLE_IRQS();
-}
 
-void tmgr_tick(void)
-{
         /* Check fast queue */
         if (fast_queue != NULL) {
-                /* Attach fast queue to main queue */
-                ANTARES_DISABLE_IRQS();
-                tmgr_task_t *fast = fast_queue,
-                           *current = main_queue,
-                           **last = &main_queue;
+
+
+                /* Save fast_queue list and make it ready for next timers.
+                 * Also, prepare main_queue for insertions.
+                 */
+                tmgr_timer_t *fast = (tmgr_timer_t *) fast_queue,
+                           *current = (tmgr_timer_t *) main_queue;
 
                 fast_queue = NULL;
-                ANTARES_ENABLE_IRQS();
 
-                while(fast != NULL)
-                {
-                        #if defined(CONFIG_LIB_TMGR_UPTIME)
-                        while(current != NULL && current->time < fast->time)
-                        #elif defined(CONFIG_LIB_TMGR_COUNTDOWN)
-                        while(current != NULL && current->time > fast->time)
-                        #endif
-                        {
+                while (fast != NULL) {
+                        while (current != NULL && current->expires < fast->expires) 
                                 current = current->next;
-                                last = &((*last)->next);
+
+                        if (!current) { /* only element in main queue */
+                                fast->prev = IN_MAIN;
+                                main_queue = fast;
+                        } else if (!current->prev) { /* if next elem in queue is root */
+                                current->prev = fast;
+                                main_queue = fast;
+                        } else {
+                                current->prev->next = fast;
+                                fast->prev = current->prev;
+                                current->prev = fast;
                         }
 
-                        fast->next = current;
-                        *last = fast;
-                        
-                        fast = fast->next;
+                        fast = fast->next; /* shift fast queue, now current element detached from fast queue */
+                        if (current)
+                                current->prev->next = current;
+                                
                 }
         }
 
         /* Run main queue tasks */
         if (main_queue != NULL) {
-                #if defined(CONFIG_LIB_TMGR_UPTIME)
-                while (main_queue != NULL && main_queue->time <= sys_uptime) {
-                #elif defined(CONFIG_LIB_TMGR_COUNTDOWN)
-                while (main_queue != NULL && main_queue->time <= 0) {
-                #endif
-                        main_queue->func();
+                while (main_queue != NULL && main_queue->expires <= sys_uptime) {
+                        void (*func)(uint8_t *) = main_queue->func;
+                        uint8_t *data = main_queue->data;
                         main_queue = main_queue->next;
+                        if (main_queue)
+                                main_queue->prev = IN_MAIN; /* dummy pointer-flag */
+
+                        ANTARES_ENABLE_IRQS(); /* run of timer function is no atomic, save interrupts! */
+                        func(data);
+                        ANTARES_DISABLE_IRQS();
                 }
         }
+        ANTARES_ENABLE_IRQS();
+}
+
+
+void tmgr_add_timer(tmgr_timer_t *timer)
+{
+        if (!timer)
+                return;
+
+        /* these lines are atomic */
+        /* TODO!!! REPLACE WITH ANTARES_ATOMIC() {} TO SAVE STATE! */
+        ANTARES_DISABLE_IRQS();
+        timer->next = (tmgr_timer_t *) fast_queue;
+        timer->prev = IN_FAST; /* set flag that this timer in fast queue */
+        
+        if (fast_queue)
+                fast_queue->prev = timer;
+
+        fast_queue = timer;
+        ANTARES_ENABLE_IRQS();
+}
+
+void tmgr_del_timer(tmgr_timer_t *timer)
+{
+        if (!timer)
+                return;
+        
+        /* these lines are atomic */
+        /* TODO!!! REPLACE WITH ANTARES_ATOMIC TO SAVE STATE! */
+        ANTARES_DISABLE_IRQS();
+        if (timer->prev == IN_MAIN) {
+                main_queue = timer->next;
+                if (main_queue)
+                        main_queue->prev = IN_MAIN;
+        } else if (timer->prev == IN_FAST) {
+                fast_queue = timer->next;
+                if (fast_queue)
+                        fast_queue->prev = IN_FAST;
+        } else if (timer->prev) {
+                timer->prev->next = timer->next;
+        }
+        ANTARES_ENABLE_IRQS();
+}
+
+void tmgr_mod_timer(tmgr_timer_t *timer, tmgr_uptime_t expires)
+{
+        if (!timer)
+                return;
+
+        /* these lines are atomic */
+        /* TODO!!! REPLACE WITH ANTARES_ATOMIC() {} TO SAVE STATE! */
+        ANTARES_DISABLE_IRQS();
+        tmgr_del_timer(timer);
+        timer->expires = expires;
+        tmgr_add_timer(timer);
+        ANTARES_ENABLE_IRQS();
 }
 
 /**
@@ -100,20 +148,15 @@ void tmgr_tick(void)
  * @{
  */
 
-tmgr_time_t tmgr_get_uptime(void)
+tmgr_uptime_t tmgr_get_uptime(void)
 {
         return sys_uptime;
 }
 
-void tmgr_delay(tmgr_time_t time)
+void tmgr_delay(tmgr_uptime_t time)
 {
-#if defined(CONFIG_LIB_TMGR_UPTIME)
         time += sys_uptime;
         while (time > sys_uptime);
-#elif defined(CONFIG_LIB_TMGR_COUNTDOWN)
-        delay = time;
-        while (delay > 0);
-#endif
 }
 
 /**
@@ -124,12 +167,12 @@ void tmgr_delay(tmgr_time_t time)
  * @section Time conversion functions
  * @{
  */
-void tmgr_set_fq(tmgr_time_t fq)
+void tmgr_set_fq(int32_t fq)
 {
         sys_fq = fq;
 }
 
-tmgr_time_t tmgr_get_fq(void)
+int32_t tmgr_get_fq(void)
 {
         return sys_fq;
 }
