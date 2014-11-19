9667cdf74bc143f2e859ca1825468b0b96cae8e5
Andrew Andrianov <andrew@ncrmnt.org>
2014-11-19 20:19:51 +0300

    esp8266: move on to esp-open-sdk
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/arch/esp8266/arch.mk
+++ src/arch/esp8266/arch.mk
@@ -1,29 +1,45 @@
 ARCH_FEATURES:=
 
 FW_FILE_1	= 0x00000
-FW_FILE_2	= 0x09000
+FW_FILE_2	= 0x0A000
 #Set our build goals
-BUILDGOALS=$(IMAGENAME).elf $(IMAGENAME)-$(FW_FILE_1).bin $(IMAGENAME)-$(FW_FILE_2).bin
+BUILDGOALS=$(IMAGENAME).rom
 
 # Do not combine objects into built-in.o
 # Failing to do so screws up things and breaks ANTARES_* macros
 # So it's 100% safe to set this to y
 LD_NO_COMBINE=y
 
-CFLAGS	  += -Wpointer-arith -Wundef -Wl,-EL -fno-inline-functions -nostdlib -mlongcalls -mtext-section-literals -D__ets__ -DICACHE_FLASH
+CFLAGS	  += -Wpointer-arith -Wundef -Wl,-EL -mtext-section-literals -fno-inline-functions -mlongcalls -D__ets__ -DICACHE_FLASH
 
 CFLAGS	  += -I$(ANTARES_INSTALL_DIR)/src/arch/esp8266/include-libc
 CFLAGS	  += -I$(ANTARES_INSTALL_DIR)/src/arch/esp8266/include-sdk
 
 GCC_LDFILE = $(ANTARES_INSTALL_DIR)/src/arch/esp8266/ld/eagle.app.v6.ld
 
-ELFFLAGS  +=  -L$(ANTARES_INSTALL_DIR)/src/arch/esp8266/lib
 
 #Link our binary blobbies
-LIBS	   =          c gcc hal phy net80211 lwip wpa main
-ELFFLAGS  +=  -Wl,--start-group $(addprefix -l,$(LIBS)) -lgcc -Wl,--end-group
 
-COMMON_LDFLAGS = -nostdlib -Wl,--no-check-sections -u call_user_start -Wl,-static
+define esp_check_lib
+ifeq ($$($(1)),y)
+ELFFLAGS+=-l$(2)
+endif
+endef
+
+ELFFLAGS  += -nostdlib -Wl,--no-check-sections -u call_user_start -Wl,-static -Wl,--start-group 
+
+$(eval $(call esp_check_lib,CONFIG_ESP8266_BLOB_MAIN,main))
+$(eval $(call esp_check_lib,CONFIG_ESP8266_BLOB_HAL,hal))
+$(eval $(call esp_check_lib,CONFIG_ESP8266_BLOB_NET80211,net80211))
+$(eval $(call esp_check_lib,CONFIG_ESP8266_BLOB_PHY,phy))
+$(eval $(call esp_check_lib,CONFIG_ESP8266_BLOB_PP,pp))
+$(eval $(call esp_check_lib,CONFIG_ESP8266_BLOB_WPA,wpa))
+$(eval $(call esp_check_lib,CONFIG_ESP8266_BLOB_SSL,ssl))
+$(eval $(call esp_check_lib,CONFIG_ESP8266_BLOB_LWIP,lwip))
+
+ELFFLAGS  += -lc -lgcc -Wl,--end-group
+
+
 LDFLAGS	  +=  $(COMMON_LDFLAGS)
 ELFFLAGS  += $(COMMON_LDFLAGS) 
 
@@ -50,5 +66,11 @@ $(IMAGENAME)-$(FW_FILE_1).bin: $(IMAGENAME).elf
 
 $(IMAGENAME)-$(FW_FILE_2).bin: $(IMAGENAME).elf
 	esptool -eo $< $(FW_FILE_2_ARGS) 
+
+$(IMAGENAME).rom: $(IMAGENAME)-$(FW_FILE_1).bin $(IMAGENAME)-$(FW_FILE_2).bin
+	dd if=/dev/zero of=$(@) bs=1K count=512
+	dd if=$(IMAGENAME)-$(FW_FILE_1).bin of=$(@) conv=notrunc
+	dd if=$(IMAGENAME)-$(FW_FILE_2).bin of=$(@) bs=1 seek=$$(($(FW_FILE_2))) 
+
 
 PHONY+=_move_code_to_irom
