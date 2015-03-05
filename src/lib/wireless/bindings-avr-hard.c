7548ef64a751e37e5c867dcae09a4e615460b435
Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>
2015-03-05 14:31:38 +0300

    rf24: Add ready to use bindings for avr software and hardware spi
    Signed-off-by: Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>

---
+++ src/lib/wireless/bindings-avr-hard.c
@@ -0,0 +1,87 @@
+#include <arch/antares.h> 
+#include <avr/io.h> 
+#include <arch/delay.h>
+#include <lib/RF24.h>
+#include <string.h>
+#include <avr/boot.h>
+#include <avr/io.h>
+#include <avr/wdt.h>
+
+#define CSN_PIN   (1<<CONFIG_CSN_PIN)
+#define  CE_PIN   (1<<CONFIG_CE_PIN)
+#define SPI_PORTX PORTB
+#define SPI_DDRX  DDRB
+#define SPI_MOSI  CONFIG_SPI_MOSI_PIN
+#define SPI_MISO  CONFIG_SPI_MISO_PIN
+#define SPI_SCK   CONFIG_SPI_SCK_PIN
+#define SPI_SS    CONFIG_SPI_SS_PIN
+
+static void set_csn(int level) 
+{
+	if (level) 
+		CSN_PORT |= CSN_PIN;
+	else
+		CSN_PORT &= ~CSN_PIN;
+}
+
+static void set_ce(int level) 
+{
+	if (level) 
+		CE_PORT |= CE_PIN;
+	else
+		CE_PORT &= ~CE_PIN;
+}
+
+
+static void spi_set_speed(int speed)
+{
+
+}
+
+static void spi_write(const uint8_t *data, uint8_t len)
+{
+	while(len--) {
+		SPDR = *data++;
+		while(!(SPSR & (1<<SPIF)));
+	}
+}
+
+static void spi_read(uint8_t *data, uint8_t len)
+{
+	while(len--) {
+		SPDR = 0xff;
+		while(!(SPSR & (1<<SPIF)));
+		*data++ = SPDR;
+	}
+}
+
+static struct rf24 r = {
+	.csn = set_csn,
+	.ce  = set_ce, 
+	.spi_set_speed = spi_set_speed, 
+	.spi_write = spi_write,
+	.spi_read  = spi_read
+};
+
+struct rf24 *g_radio = &r;
+
+ANTARES_INIT_LOW(avr_hardspi_init)
+{
+	CSN_DDR |= CSN_PIN;
+	CE_DDR  |=  CE_PIN;
+
+	SPI_DDRX &= ~(1<<SPI_MISO);
+	SPI_DDRX |= (1<<SPI_MOSI)|(1<<SPI_SCK)|(1<<SPI_SS);
+	SPI_PORTX |= (1<<SPI_MOSI)|(1<<SPI_SCK)|(1<<SPI_MISO)|(1<<SPI_SS);
+	
+
+	SPCR = ((1<<SPE)|               /* SPI Enable */
+		(0<<SPIE)|              /* SPI Interrupt Enable */
+		(0<<DORD)|              /* Data Order (0:MSB first / 1:LSB first) */
+		(1<<MSTR)|              /* Master/Slave select */   
+		(0<<SPR1)|(0<<SPR0)|    /* SPI Clock Rate */
+		(0<<CPOL)|              /* Clock Polarity (0:SCK low / 1:SCK hi when idle) */
+		(0<<CPHA));             /* Clock Phase (0:leading / 1:trailing edge sampling) */
+
+	SPSR = (1<<SPI2X);              /* Double Clock Rate */
+}
---
+++ src/lib/wireless/bindings-avr-soft.c
@@ -0,0 +1,97 @@
+#include <arch/antares.h> 
+#include <avr/io.h> 
+#include <arch/delay.h>
+#include <lib/RF24.h>
+#include <string.h>
+#include <avr/boot.h>
+#include <avr/wdt.h>
+#include <avr/sfr_defs.h>
+
+#define CSN_PIN   (1<<CONFIG_CSN_PIN)
+#define  CE_PIN   (1<<CONFIG_CE_PIN)
+#define SPI_PORTX PORTB
+#define SPI_DDRX  DDRB
+#define SPI_MOSI  CONFIG_SPI_MOSI_PIN
+#define SPI_MISO  CONFIG_SPI_MISO_PIN
+#define SPI_SCK   CONFIG_SPI_SCK_PIN
+#define SPI_SS    CONFIG_SPI_SS_PIN
+
+static void set_csn(int level) 
+{
+	if (level) 
+		CSN_PORT |= CSN_PIN;
+	else
+		CSN_PORT &= ~CSN_PIN;
+}
+
+static void set_ce(int level) 
+{
+	if (level) 
+		CE_PORT |= CE_PIN;
+	else
+		CE_PORT &= ~CE_PIN;
+}
+
+
+static void spi_set_speed(int speed)
+{
+
+}
+
+#define SCK_ZERO  SPI_PORTX &= ~(1<<SPI_SCK)
+#define SCK_ONE   SPI_PORTX |=  (1<<SPI_SCK)
+#define MOSI_ZERO SPI_PORTX &= ~(1<<SPI_MOSI)
+#define MOSI_ONE  SPI_PORTX |=  (1<<SPI_MOSI)
+
+
+#define sbi(sfr, bit) sfr |= _BV(bit)
+#define cbi(sfr, bit) sfr &= ~_BV(bit)
+
+static uint8_t spi_xfer(uint8_t data)
+{
+	uint8_t recv=0;
+	int i;
+	for (i=7; i>=0; i--) {
+		SCK_ZERO;
+		if (data & (1<<i))
+			MOSI_ONE;
+		else
+			MOSI_ZERO;
+		SCK_ONE;
+		recv |= (SPI_PINX & (1<<SPI_MISO)) ? (1 << i) : 0;
+	}
+	SCK_ZERO;
+	return recv;
+}
+
+static void spi_write(const uint8_t *data, uint8_t len)
+{
+	while(len--)
+		spi_xfer(*data++);
+}
+
+static void spi_read(uint8_t *data, uint8_t len)
+{
+	while(len--)
+		*data++ = spi_xfer(0xff);
+}
+
+static struct rf24 r = {
+	.csn = set_csn,
+	.ce  = set_ce, 
+	.spi_set_speed = spi_set_speed, 
+	.spi_write = spi_write,
+	.spi_read  = spi_read
+};
+
+struct rf24 *g_radio = &r;
+
+ANTARES_INIT_LOW(platform_setup)
+{	
+	CSN_DDR |= CSN_PIN;
+	CE_DDR  |=  CE_PIN;
+	SPI_DDRX |= (1<<SPI_MOSI) | (1<<SPI_SCK) |(1<<SPI_SS);
+	SPI_DDRX &= ~(1<<SPI_MISO);
+	SPI_PORTX |= (1<<SPI_MOSI)|(1<<SPI_SCK)|(1<<SPI_SS);
+	SCK_ZERO;
+}
